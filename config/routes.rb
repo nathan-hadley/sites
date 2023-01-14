c9f361fe26989d35cd9203ee5e7c59eabd340c04
Nathan Hadley <nathan.hadley@me.com>
2023-01-14 15:31:33 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'photos/index'
+  get 'about/index'
+  get 'stories/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
