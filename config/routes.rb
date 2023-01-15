c22abaf847888bfb601c80ef0ac23289b780b7f3
Nathan Hadley <nathan.hadley@me.com>
2023-01-14 16:38:34 -0800

    rough in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  root 'stories#index'
   get 'photos/index'
   get 'about/index'
   get 'stories/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
