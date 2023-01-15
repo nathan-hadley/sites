ba1bf393d56bffcf6fa5b8f0342c10a4bd055619
Nathan Hadley <nathan.hadley@me.com>
2023-01-14 23:42:41 -0800

    initial work on photos page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
   def index
+    photo_paths = Dir["app/assets/images/photos/*"]
+    @photos = photo_paths.map { |file| File.basename(file) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'stories#index'
+
+  get 'stories/index'
   get 'photos/index'
   get 'about/index'
-  get 'stories/index'
 end
