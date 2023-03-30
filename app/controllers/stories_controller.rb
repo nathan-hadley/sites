f08a03b35a8bdceab376f9bb5b016b76ac3afec1
Nathan Hadley <nathan.hadley@me.com>
2023-03-30 10:10:13 -0400

    add dates to archive and update about page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -11,7 +11,15 @@ class StoriesController < ApplicationController
 
   private
 
+  # Done before both index (paginated articles) and show (individual article).
   def set_articles
-    @articles = Article.all
+    articles = Article.all
+    articles_within_years = {}
+    articles.each do |article|
+      year = article.publish_date.strftime("%Y")
+      articles_within_years[year] ||= []
+      articles_within_years[year] << article
+    end
+    @articles_within_years = articles_within_years
   end
 end
