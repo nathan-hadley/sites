b5d5eba76d183b6fbcff920670995a79d98e1412
Nathan Hadley <nathan.hadley@me.com>
2023-09-28 09:37:41 -0700

    fix scroll to archive and add meta tags to stories

--- app/javascript/controllers/archive_controller.js
+++ app/javascript/controllers/archive_controller.js
@@ -3,8 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   scroll(event) {
     event.preventDefault()
+    const archiveSection = document.getElementById("archive");
     window.scrollTo({
-      top: document.body.scrollHeight,
+      top: archiveSection.offsetTop - 40,
       behavior: "smooth"
     });
   }
